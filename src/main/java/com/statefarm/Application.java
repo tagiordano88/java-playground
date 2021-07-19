813f5be14578c98819cfadac669d66115f69b8d0
tagiordano88 <tiffany.giordano.j0ho@statefarm.com>
2021-07-19 15:17:08 -0500

    added code for command line

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -3,10 +3,12 @@ package com.statefarm;
 public class Application {
 
     public static void main(String[] args) {
-       for (int i = 2; i <= 6; i++) {
-           if (i % 2 == 0) {
-               System.out.println(i);
-           }
+       if (args.length < 2 && args.length >0) {
+           System.out.println("Please specify an email for " + args[0]);
+       } else if (args.length < 2) {
+           System.out.println("Please specify a name and email");
+       } else {
+           System.out.println(args[0] + " " + '<' + args[1] + '>');
        }
     }
 }
